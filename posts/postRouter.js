fb79b2fe2097eaabfeb660241698a4813732bd74
Jessica <duellje@oregonstate.edu>
2020-08-05 18:52:01 -0400

    post delete works - stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,60 @@
-const express = require('express');
+const express = require("express");
+const PostData = require("./postDb");
+const { post } = require("../users/userRouter");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  PostData.get()
+    .then((response) => {
+      res.status(200).json({ response });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "There is no posts available" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validatePostId, (req, res) => {
+  try {
+    PostData.getById(req.post).then((thePost) => {
+      res.status(200).json({ data: thePost });
+    });
+  } catch {
+    res
+      .status(500)
+      .json({ errorMessage: "Could not retrieve post from database" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validatePostId, (req, res) => {
+  try {
+    PostData.remove(req.post).then((deleted) => {
+      res.status(200).json({ message: "deleted post" });
+    });
+  } catch {
+    res
+      .status(500)
+      .json({ errorMessage: "Could not delete post from database" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const postId = req.params.id;
+
+  PostData.getById(postId)
+    .then((post) => {
+      req.post = post.id;
+      next();
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "invalid post id" });
+    });
 }
 
 module.exports = router;
